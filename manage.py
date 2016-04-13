91f66ecce3aa90fbbf0379f2cffd762c90643647
jason <295024171@qq.com>
2016-04-13 15:27:00 +0800

    4

--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
+import os
 from flask.ext.script import Manager,Shell
-from app import create_app
+from app import create_app,db
+from flask.ext.migrate import Migrate, MigrateCommand
+from app.models import User, Role
 
-
-app=create_app()
+app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
-	return dict(app=app)
-
+	return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command('sehll',Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
 	manager.run()
+	
