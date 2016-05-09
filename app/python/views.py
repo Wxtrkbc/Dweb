847ea4a36391af13534770fad3af7d5f615a6d1c
jason <295024171@qq.com>
2016-05-09 11:01:14 +0800

    boke11

--- app/python/views.py
+++ app/python/views.py
@@ -9,3 +9,31 @@ def py1():
 @python.route('/1-2')
 def py2():
 	return render_template('python/1-2.html')
+
+@python.route('/1-3')
+def py3():
+	return render_template('python/1-3.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@python.route('/sz1')
+def sz1():
+	return render_template('python/sz1.html')
+
+
+@python.route('/ym1')
+def ym1():
+	return render_template('python/ym1.html')
